69ef8808ac25a7e4e8b2fa443dc9e45cd5f1be33
IT521 <terrysfrancis@hotmail.com>
2019-07-24 16:28:56 -0400

    Export Storybook as a static app

---
+++ storybook-static/precache-manifest.f02b1608a552818f06bccb24f44836d8.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579d84d8f1570a5eb4164d8d24a581eb",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c837930521ee7bde8e79.bundle.js"
+  },
+  {
+    "revision": "bc914254b81982a5f281",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.c837930521ee7bde8e79.bundle.js"
+  },
+  {
+    "url": "vendors~main.c837930521ee7bde8e79.bundle.js"
+  }
+]);
